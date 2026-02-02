ee895177fb5cfc5f8916d7bbd77a7cad6b32b523
BoomTHDev <minecraft8231@gmail.com>
2026-02-03 02:15:17 +0700

    implement database schema

---
+++ src/db/db.js
@@ -0,0 +1,14 @@
+import "@dotenvx/dotenvx/config";
+
+import { drizzle } from "drizzle-orm/neon-http";
+import pg from "pg";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined.");
+}
+
+export const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool);
